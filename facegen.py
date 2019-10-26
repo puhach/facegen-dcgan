b05a744a254218770a32516eff2f817681dd7d7a
Yaroslav Pugach <pugach.yaroslav2@gmail.com>
2019-10-26 19:12:11 +0300

    feat: Implement data loading

--- facegen.py
+++ facegen.py
@@ -1,9 +1,34 @@
 import argparse
+import torch
+from torchvision import datasets
+from torchvision import transforms
+
+
+def get_dataloader(batch_size, image_size, data_dir):
+    """
+    Batch the neural network data using DataLoader
+    :param batch_size: The number of images in a batch
+    :param img_size: The square size to resize the input images to
+    :param data_dir: Directory where image data is located
+    :return: DataLoader with batched data
+    """
+    
+    preprocessing = transforms.Compose([transforms.Resize((image_size, image_size)),                                         
+                                        transforms.ToTensor()])
+
+    dataset = datasets.ImageFolder(root=data_dir, transform=preprocessing)
+
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=0)
+    
+    return dataloader
+
 
 
 def train(args):
+    print("Loading data")
     #print(f"training for {args.epochs} epochs with a learning rate = {args.lr}")
-    pass
+    dataloader = get_dataloader(batch_size=64, image_size=32, data_dir='celeba')
+
 
 def generate(args):
     #print(f"generate to {args.path}")
@@ -25,7 +50,7 @@ parser_gen.add_argument('-path', type=str, required=True,
     help='The path to the file where the generated image has to be stored.')
 parser_gen.set_defaults(func=generate)
 
-#args = parser.parse_args("train -lr 0.001 -epochs=4".split())
+args = parser.parse_args("train -lr 0.001 -epochs=4".split())
 #args = parser.parse_args("generate -path z:/test.jpg".split())
-args = parser.parse_args()
+#args = parser.parse_args()
 args.func(args)
