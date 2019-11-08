1e1bcd7dbe5b2484e5a7c4d25fae3a18a48ec5ef
Yaroslav Pugach <pugach.yaroslav2@gmail.com>
2019-11-08 21:38:00 +0200

    feat: Implement the training routine

--- facegen.py
+++ facegen.py
@@ -117,6 +117,96 @@ def fake_loss(D_out):
     loss = torch.mean((D_out - 0)**2)
     return loss
 
+def run_training(D, G, n_epochs, print_every=50):
+    """
+    Trains the adversarial networks for the specified number of epochs.
+    :param D: The discriminator network.
+    :param G: The generator network.
+    :param n_epochs: The number of epochs to train for.
+    :param print_every: Controls how often to print and record the models' losses.
+    :return: D and G losses.
+    """
+    
+    # move models to GPU
+    if train_on_gpu:
+        D.cuda()
+        G.cuda()
+
+    # keep track of loss and generated, "fake" samples
+    samples = []
+    losses = []
+
+    # Get some fixed data for sampling. These are images that are held
+    # constant throughout training, and allow us to inspect the model's performance
+    sample_size=16
+    fixed_z = np.random.uniform(-1, 1, size=(sample_size, z_size))
+    fixed_z = torch.from_numpy(fixed_z).float()
+    # move z to GPU if available
+    if train_on_gpu:
+        fixed_z = fixed_z.cuda()
+
+    # epoch training loop
+    for epoch in range(n_epochs):
+
+        # batch training loop
+        for batch_i, (real_images, _) in enumerate(celeba_train_loader):
+
+            batch_size = real_images.size(0)
+            real_images = scale(real_images)
+
+            if train_on_gpu:
+                real_images = real_images.cuda()
+            
+            # 1. Train the discriminator on real and fake images
+            #d_out = D(real_images)
+            d_real_loss = real_loss(D(real_images))
+            
+            z = torch.from_numpy(np.random.uniform(-1, 1, size=(batch_size, z_size))).float()
+            if train_on_gpu:
+                z = z.cuda()
+                
+            d_fake_loss = fake_loss(D(G(z)))
+            
+            d_loss = d_real_loss + d_fake_loss
+            
+            d_optimizer.zero_grad()
+            d_loss.backward()
+            d_optimizer.step()
+
+            # 2. Train the generator with an adversarial loss
+            z = torch.from_numpy(np.random.uniform(-1, 1, size=(batch_size, z_size))).float()
+            if train_on_gpu:
+                z = z.cuda()
+                
+            g_loss = real_loss(D(G(z)))
+            
+            g_optimizer.zero_grad()
+            g_loss.backward()
+            g_optimizer.step()
+            
+            
+            # Print some loss stats
+            if batch_i % print_every == 0:
+                # append discriminator loss and generator loss
+                losses.append((d_loss.item(), g_loss.item()))
+                # print discriminator and generator loss
+                print('Epoch [{:5d}/{:5d}] | d_loss: {:6.4f} | g_loss: {:6.4f}'.format(
+                        epoch+1, n_epochs, d_loss.item(), g_loss.item()))
+
+
+        # after each epoch generate and save sample (fake images)
+        G.eval() # for generating samples
+        samples_z = G(fixed_z)
+        samples.append(samples_z)
+        G.train() # back to training mode
+
+    # Save training generator samples
+    with open('train_samples.pkl', 'wb') as f:
+        pkl.dump(samples, f)
+    
+    # finally return losses
+    return losses
+
 
 def train(args):
 
